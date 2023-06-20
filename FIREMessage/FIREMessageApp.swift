a6a2579d232da7ed8a384d5ed79b04c52fc4c7e8
cilippofilia <filippocarlocilia@gmail.com>
2023-06-20 10:41:18 +0100

    Initial Commit

---
+++ FIREMessage/FIREMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIREMessageApp.swift
+//  FIREMessage
+//
+//  Created by Filippo Cilia on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FIREMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
